f15bcc5dcd0ee2d732c0048904e60c70de7b7c52
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-10 15:14:35 +0000

    Added some comments.

--- gethtmlchunks.js
+++ gethtmlchunks.js
@@ -7,10 +7,12 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
+  // Call the get method, pass the options needed to get the data and give the callback the response.
   https.get(requestOptions, function (response) {
 
     response.setEncoding('utf-8');
 
+    // Show the chunks of data as they are STREAMING in.
     response.on('data', function(data) {
       console.log("Chunk ---> " + data + "\n")
     });
