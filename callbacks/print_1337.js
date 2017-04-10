1d2a4d365ea3d8e66bb984d2bafe6a09a73645f3
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-10 16:58:04 +0000

    Created code for the stretch. It converts the text coming from and https stream into leetspeak 90s hacker code.

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,8 +1,38 @@
 
 var getHTML = require('../http-functions');
 
-function  () {
+function leetspeak (aString) {
+  // Use a RegExp to find the letters and a function to custom replace them.
+  var hacked = aString.replace(/a|e|o|l|s|t|[ck]|[er]|[you]/gi, function myFunction(x) {
 
+    if (x === 'a')
+      return '4';
+    else if (x === 'e')
+      return '3';
+    else if (x === 'o')
+      return '0';
+    else if (x === 'l')
+      return '1';
+    else if (x === 's')
+      return '5';
+    else if (x === 't')
+      return '7';
+    else if (x === 'ck')
+      return 'x';
+    else if (x === 'er')
+      return '0r';
+    else if (x === 'you')
+      return 'j00';
+    else {
+      return x;
+    }
+  })
+
+  return hacked;
+}
+
+function print1337(html) {
+  console.log(leetspeak(html));
 }
 
 var requestOptions = {
@@ -10,4 +40,4 @@ var requestOptions = {
   path: '/http-examples/step6/1337.html'
 };
 
-console.log(getHTML(requestOptions, printHTML));
+console.log(getHTML(requestOptions, print1337));
